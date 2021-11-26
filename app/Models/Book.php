d91b3e05f84e1a1e604194ef46e46b3587b9a4c9
StevRomo420 <esteban5420978@hotmail.com>
2021-11-25 22:58:55 -0800

    Se agregan las clases de el caso1

---
+++ app/Models/Book.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected string $author;
+    protected string $title;
+    protected array $index;
+    
+}
